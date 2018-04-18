e6b54ce7f1da62ba880e1ef38793b98114301778
wojteki <w.kiezik@gmail.com>
2018-04-18 15:50:35 +0200

    done

--- src/helper_functions/formObjectRenderingArray.js
+++ src/helper_functions/formObjectRenderingArray.js
@@ -14,58 +14,48 @@ export const formObjectRenderingArray = function(state, rootQuestionsOrder) {
             }
         });
     }
+
+    const questionRecursiveCall = function(questionId) {
+        let testPassed = false;
+        const parentId = state[questionId].parentId;
         
-    if (state && rootQuestionsOrder) {
+        if (state[questionId].parentId === 'rootNode') { testPassed = true; }
+        
+        if(state[questionId].parentId !== 'rootNode') {
+            let parentAnswer = state[parentId].answer;
+            let conditionalValue = state[parentId].conditionalQuestions[questionId].value;
+            const conditionType = state[parentId].conditionalQuestions[questionId].type;
 
-        const questionRecursiveCall = function(questionId) {
-            let testPassed = false;
-            const parentId = state[questionId].parentId;
-            
-            if (state[questionId].parentId === 'rootNode') { testPassed = true; }
-            
-            if(state[questionId].parentId !== 'rootNode') {
-                let parentAnswer = state[parentId].answer;
-                let conditionalValue = state[parentId].conditionalQuestions[questionId].value;
-                const conditionType = state[parentId].conditionalQuestions[questionId].type;
+            if (conditionType === 'equals') {
+                parentAnswer = parentAnswer.toString().toLowerCase();
+                conditionalValue = conditionalValue.toString().toLowerCase();
 
-                if (conditionType === 'equals') {
-                    parentAnswer = parentAnswer.toString().toLowerCase();
-                    conditionalValue = conditionalValue.toString().toLowerCase();
+                if (parentAnswer === conditionalValue && parentAnswer !== '') { testPassed = true; } 
 
-                    if (parentAnswer === conditionalValue && parentAnswer !== '') { 
-                        testPassed = true; 
-                    } else { checkIfConditionalQuestionsShouldRender(); }
+            } else if (conditionType === 'greater') {
+                if(parentAnswer !== '') { parentAnswer = Number(parentAnswer); }
+                conditionalValue = Number(conditionalValue);
 
-                } else if (conditionType === 'greater') {
-                    if(parentAnswer !== '') { parentAnswer = Number(parentAnswer); }
-                    conditionalValue = Number(conditionalValue);
+                if ((parentAnswer > conditionalValue) && parentAnswer !== '') { testPassed = true; } 
 
-                    if ((parentAnswer > conditionalValue) && parentAnswer !== '') { 
-                        testPassed = true; 
-                    } else { checkIfConditionalQuestionsShouldRender(); }
+            } else if (conditionType === 'less') {
+                if(parentAnswer !== '') { parentAnswer = Number(parentAnswer); }
+                conditionalValue = Number(conditionalValue);
 
-                } else if (conditionType === 'less') {
-                    if(parentAnswer !== '') { parentAnswer = Number(parentAnswer); }
-                    conditionalValue = Number(conditionalValue);
-
-                    if ((parentAnswer < conditionalValue) && parentAnswer !== '') { 
-                        testPassed = true; 
-                    } else { checkIfConditionalQuestionsShouldRender(); } 
-                }
-                
-
-                // console.table({parent: parentId, parentAnswer: parent.answer, conditionalQuestionId: questionId, conditionalAnswer: conditionalValue});
-            }
-
-            if(testPassed) { questionsRenderArray.push(questionId); }
-
-            if (Object.keys(state[questionId].conditionalQuestions)[0]) {
-                for(let key in state[questionId].conditionalQuestions) { questionRecursiveCall(key); }
+                if ((parentAnswer < conditionalValue) && parentAnswer !== '') { testPassed = true; } 
             }
         }
-        
-        rootQuestionsOrder.map(rootQuestion => { questionRecursiveCall(rootQuestion); });
-        
-        return questionsRenderArray;
+
+        if(testPassed) { questionsRenderArray.push(questionId); }
+
+        if (Object.keys(state[questionId].conditionalQuestions)[0]) {
+            for(let key in state[questionId].conditionalQuestions) { questionRecursiveCall(key); }
+        }
     }
+    
+    rootQuestionsOrder.map(rootQuestion => { questionRecursiveCall(rootQuestion); });
+    
+    checkIfConditionalQuestionsShouldRender();
+
+    return questionsRenderArray;
 }
