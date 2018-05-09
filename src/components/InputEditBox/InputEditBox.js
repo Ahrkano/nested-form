294cf97693304b78357a8c729418bc31650d5c4d
wojteki <w.kiezik@gmail.com>
2018-05-09 12:25:20 +0200

    checkpoint

--- src/components/InputEditBox/InputEditBox.js
+++ src/components/InputEditBox/InputEditBox.js
@@ -1,114 +1,20 @@
 import React, { Component } from 'react';
-import Aux from '../../hoc/Auxiliary/Auxiliary';
-import './InputEditBox.css';
+import {
+    returnCondition,
+    returnQuestionInput,
+    returnQuestionType,
+    returnButtons
+} from './services/services';
 
-import InputButton from '../Buttons/InputButton/InputButton';
+import './InputEditBox.css';
 
 class InputEditBox extends Component {
     render() {
         const marginLeft = 40 * this.props.level + 'px';
-        let condition = null;
-
-        if (this.props.condition !== 'noCondition') {
-            if (this.props.parentType === 'text') {
-                condition = (
-                    <Aux>
-                        <select
-                            id="questionType"
-                            value={this.props.condition}
-                            onChange={event =>
-                                this.props.onInputChange(event, this.props.id, 'condition')
-                            }
-                        >
-                            <option value="equals">Equals</option>
-                        </select>
-                        <input
-                            type="text"
-                            id="questionInput"
-                            value={this.props.conditionValue}
-                            onChange={event =>
-                                this.props.onInputChange(event, this.props.id, 'conditionValue')
-                            }
-                        />
-                    </Aux>
-                );
-            } else if (this.props.parentType === 'yesNo') {
-                condition = (
-                    <Aux>
-                        <select
-                            id="questionType"
-                            value={this.props.condition}
-                            onChange={event =>
-                                this.props.onInputChange(event, this.props.id, 'condition')
-                            }
-                        >
-                            <option value="equals">Equals</option>
-                        </select>
-                        <select
-                            id="questionType"
-                            value={this.props.conditionValue}
-                            onChange={event =>
-                                this.props.onInputChange(event, this.props.id, 'conditionValue')
-                            }
-                        >
-                            <option value="yes">Yes</option>
-                            <option value="no">No</option>
-                        </select>
-                    </Aux>
-                );
-            } else {
-                condition = (
-                    <Aux>
-                        <select
-                            id="questionType"
-                            value={this.props.condition}
-                            onChange={event =>
-                                this.props.onInputChange(event, this.props.id, 'condition')
-                            }
-                        >
-                            <option value="equals">Equals</option>
-                            <option value="greater">Greater than</option>
-                            <option value="less">Less than</option>
-                        </select>
-                        <input
-                            className="InputEditBox__number-input"
-                            type="number"
-                            id="questionInput"
-                            value={this.props.conditionValue}
-                            onChange={event =>
-                                this.props.onInputChange(event, this.props.id, 'conditionValue')
-                            }
-                        />
-                    </Aux>
-                );
-            }
-        }
-
-        const questionInput = (
-            <div className="InputEditBox__question-value">
-                <label htmlFor="questionInput">Question: </label>
-                <input
-                    id="questionInput"
-                    value={this.props.value}
-                    onChange={event => this.props.onInputChange(event, this.props.id, 'question')}
-                />
-            </div>
-        );
-
-        const type = (
-            <div className="InputEditBox__question-type">
-                <label htmlFor="questionType">Type: </label>
-                <select
-                    id="questionType"
-                    value={this.props.type}
-                    onChange={event => this.props.onInputChange(event, this.props.id, 'type')}
-                >
-                    <option value="yesNo">Yes / No</option>
-                    <option value="text">Text</option>
-                    <option value="number">Number</option>
-                </select>
-            </div>
-        );
+        const condition = returnCondition(this.props);
+        const questionInput = returnQuestionInput(this.props);
+        const type = returnQuestionType(this.props);
+        const buttons = returnButtons(this.props);
 
         return (
             <div className="InputEditBox" style={{ marginLeft: marginLeft }}>
@@ -124,28 +30,7 @@ class InputEditBox extends Component {
                     {questionInput}
                     {type}
                 </div>
-                <div className="InputEditBox__buttons">
-                    <InputButton
-                        className="InputButton__delete"
-                        onButtonClick={() =>
-                            this.props.onInputDeletion(this.props.id, this.props.parent)
-                        }
-                    >
-                        Delete
-                    </InputButton>
-                    <InputButton
-                        className="InputButton__add-sub-input"
-                        onButtonClick={() =>
-                            this.props.onSubInputAddition(
-                                this.props.id,
-                                this.props.level,
-                                this.props.type
-                            )
-                        }
-                    >
-                        Add Sub-Input
-                    </InputButton>
-                </div>
+                {buttons}
             </div>
         );
     }
---
+++ src/components/InputEditBox/services/services.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
+import InputButton from '../../Buttons/InputButton/InputButton';
+
+export const returnCondition = props => {
+    if (props.condition !== 'noCondition') {
+        switch (props.parentType) {
+            case 'text':
+                return (
+                    <Aux>
+                        <select
+                            id="questionType"
+                            value={props.condition}
+                            onChange={event => props.onInputChange(event, props.id, 'condition')}
+                        >
+                            <option value="equals">Equals</option>
+                        </select>
+                        <input
+                            type="text"
+                            id="questionInput"
+                            value={props.conditionValue}
+                            onChange={event =>
+                                props.onInputChange(event, props.id, 'conditionValue')
+                            }
+                        />
+                    </Aux>
+                );
+            case 'yesNo':
+                return (
+                    <Aux>
+                        <select
+                            id="questionType"
+                            value={props.condition}
+                            onChange={event => props.onInputChange(event, props.id, 'condition')}
+                        >
+                            <option value="equals">Equals</option>
+                        </select>
+                        <select
+                            id="questionType"
+                            value={props.conditionValue}
+                            onChange={event =>
+                                props.onInputChange(event, props.id, 'conditionValue')
+                            }
+                        >
+                            <option value="yes">Yes</option>
+                            <option value="no">No</option>
+                        </select>
+                    </Aux>
+                );
+            default:
+                return (
+                    <Aux>
+                        <select
+                            id="questionType"
+                            value={props.condition}
+                            onChange={event => props.onInputChange(event, props.id, 'condition')}
+                        >
+                            <option value="equals">Equals</option>
+                            <option value="greater">Greater than</option>
+                            <option value="less">Less than</option>
+                        </select>
+                        <input
+                            className="InputEditBox__number-input"
+                            type="number"
+                            id="questionInput"
+                            value={props.conditionValue}
+                            onChange={event =>
+                                props.onInputChange(event, props.id, 'conditionValue')
+                            }
+                        />
+                    </Aux>
+                );
+        }
+    }
+};
+
+export const returnQuestionInput = props => {
+    return (
+        <div className="InputEditBox__question-value">
+            <label htmlFor="questionInput">Question: </label>
+            <input
+                id="questionInput"
+                value={props.value}
+                onChange={event => props.onInputChange(event, props.id, 'question')}
+            />
+        </div>
+    );
+};
+
+export const returnQuestionType = props => {
+    return (
+        <div className="InputEditBox__question-type">
+            <label htmlFor="questionType">Type: </label>
+            <select
+                id="questionType"
+                value={props.type}
+                onChange={event => props.onInputChange(event, props.id, 'type')}
+            >
+                <option value="yesNo">Yes / No</option>
+                <option value="text">Text</option>
+                <option value="number">Number</option>
+            </select>
+        </div>
+    );
+};
+
+export const returnButtons = props => {
+    return (
+        <div className="InputEditBox__buttons">
+            <InputButton
+                className="InputButton__delete"
+                onButtonClick={() => props.onInputDeletion(props.id, props.parent)}
+            >
+                Delete
+            </InputButton>
+            <InputButton
+                className="InputButton__add-sub-input"
+                onButtonClick={() => props.onSubInputAddition(props.id, props.level, props.type)}
+            >
+                Add Sub-Input
+            </InputButton>
+        </div>
+    );
+};
